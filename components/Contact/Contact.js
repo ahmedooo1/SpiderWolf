b0b842f00820d5daee6844bcf6d80575ed621491
ahmedooo1 <94185300+ahmedooo1@users.noreply.github.com>
2023-02-12 17:04:12 +0100

    update repo

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -3,48 +3,48 @@ import cardimg2 from '../../public/spider1.png';
 
 function ContactComponent() {
     return (
-      <div class="bg-gray-900 max-h-full flex items-center justify-center w-full ">
-        <div class=" w-full max-w-sm ">
-          <div class="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 m-4">
-          <Image src={cardimg2} alt="Service 1" class="w-full h-40 rounded-md"/> 
+      <div className="bg-gray-900 max-h-full flex items-center justify-center w-full ">
+        <div className=" w-full max-w-sm ">
+          <div className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 m-4">
+          <Image src={cardimg2} alt="Service 1" className="w-full h-40 rounded-md"/> 
 
-            <h1 class="text-3xl font-bold text-center mb-6">Nous contacter</h1>
-            <form class="w-full">
-              <div class="mb-4">
-                <label class="block text-gray-200 font-bold mb-2" for="name">
+            <h1 className="text-3xl font-bold text-center mb-6">Nous contacter</h1>
+            <form className="w-full">
+              <div className="mb-4">
+                <label className="block text-gray-200 font-bold mb-2" for="name">
                   Nom
                 </label>
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="name"
                   type="text"
                   placeholder="Nom"
                 />
               </div>
-              <div class="mb-4">
-                <label class="block text-gray-200 font-bold mb-2" for="email">
+              <div className="mb-4">
+                <label className="block text-gray-200 font-bold mb-2" for="email">
                   Email
                 </label>
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   type="email"
                   placeholder="Email"
                 />
               </div>
-              <div class="mb-4">
-                <label class="block text-gray-200 font-bold mb-2" for="message">
+              <div className="mb-4">
+                <label className="block text-gray-200 font-bold mb-2" for="message">
                   Message
                 </label>
                 <textarea
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
                   id="message"
                   placeholder="Message"
                 />
               </div>
-              <div class="flex items-center justify-center">
+              <div className="flex items-center justify-center">
                 <button
-                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="submit"
                 >
                   Envoyer
--- components/Home/SectionCards.js
+++ components/Home/SectionCards.js
@@ -5,36 +5,36 @@ import cardimg3 from '../../public/Wolf-face.png';
 function SectionCards(){
     return(<>
     {/* la deuxieme sections dans home page (les cards) */}
-    <section class="dark:bg-gray-900 py-12">
-<div class="container mx-auto px-6">
-  <h2 class="text-3xl font-bold mb-8">Nos services</h2>
-  <div class="flex flex-wrap -mx-4">
-    <div class="w-full md:w-1/3 px-4 mb-8  ">
-      <div class="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
-        <Image src={cardimg1} alt="Service 1" class="w-full h-60"/> 
-        <div class="px-6 py-4">
-          <h3 class="text-xl font-bold mb-2">Service 1</h3>
-          <p class="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
+    <section className="dark:bg-gray-900 py-12">
+<div className="container mx-auto px-6">
+  <h2 className="text-3xl font-bold mb-8">Nos services</h2>
+  <div className="flex flex-wrap -mx-4">
+    <div className="w-full md:w-1/3 px-4 mb-8  ">
+      <div className="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
+        <Image src={cardimg1} alt="Service 1" className="w-full h-60"/> 
+        <div className="px-6 py-4">
+          <h3 className="text-xl font-bold mb-2">Service 1</h3>
+          <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
         </div>
       </div>
     </div>
-    <div class="w-full md:w-1/3 px-4 mb-8 ">
-      <div class="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
-      <Image src={cardimg2} alt="Service 1" class="w-full h-60"/> 
+    <div className="w-full md:w-1/3 px-4 mb-8 ">
+      <div className="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
+      <Image src={cardimg2} alt="Service 1" className="w-full h-60"/> 
 
-        <div class="px-6 py-4">
-          <h3 class="text-xl font-bold mb-2">Service 2</h3>
-          <p class="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
+        <div className="px-6 py-4">
+          <h3 className="text-xl font-bold mb-2">Service 2</h3>
+          <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
         </div>
       </div>
     </div>
-    <div class="w-full md:w-1/3 px-4 mb-8 ">
-      <div class="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
-      <Image src={cardimg3} alt="Service 1" class="w-full h-60"/> 
+    <div className="w-full md:w-1/3 px-4 mb-8 ">
+      <div className="bg-gradient-to-br from-pink-400 to-blue-600 cursor-pointer rounded shadow-lg hover:shadow-2xl transition-all duration-300">
+      <Image src={cardimg3} alt="Service 1" className="w-full h-60"/> 
 
-        <div class="px-6 py-4">
-          <h3 class="text-xl font-bold mb-2">Service 3</h3>
-          <p class="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
+        <div className="px-6 py-4">
+          <h3 className="text-xl font-bold mb-2">Service 3</h3>
+          <p className="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo, est eget bibendum commodo, velit velit convallis velit, at tempor nulla ipsum eget metus.</p>
         </div>
       </div>
     </div>
